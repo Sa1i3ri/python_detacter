451b6339828b1a0f86739860235f23464b0b6491
yangyouwen141036 <1483224757@qq.com>
2024-04-23 14:02:45 +0800

    add:train 20-39

--- read_data.py
+++ read_data.py
@@ -2,15 +2,15 @@ import pandas as pd
 import os
 
 test = pd.read_parquet('defectors\jit_bug_prediction_splits\\random\\test.parquet.gzip')
-train = pd.read_parquet('defectors\jit_bug_prediction_splits\\random\\train.parquet.gzip')
+train = pd.read_parquet('defectors\line_bug_prediction_splits\\random\\train.parquet.gzip')
 val = pd.read_parquet('defectors\jit_bug_prediction_splits\\random\\train.parquet.gzip')
 
 # 确保输出文件夹存在
 output_folder = 'dataSet'
 os.makedirs(output_folder, exist_ok=True)
 
-start = 10
-num = 10
+start = 0
+num = 40
 
 # 遍历数据并写入单独的文件
 for index in range(start,start+num):
@@ -18,8 +18,10 @@ for index in range(start,start+num):
     # 生成文件名，这里可以根据需要自定义文件名格式
     filename = os.path.join(output_folder, f'train_{index}.txt')
     # 写入文件
-    with open(filename, 'a') as file:
+    with open(filename, 'w',encoding='utf-8') as file:
         file.write(row.to_string())
-        file.write(train.iloc[index,4])
+        file.write("\n")
+        file.write("\n")
+        file.write(train.iloc[index,4].decode('utf-8'))
 
 
